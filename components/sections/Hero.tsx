ca31b2284eec57940f36d1d291efc9235d0d87a1
Rufaro <80164120+rufaro-ndhlovu@users.noreply.github.com>
2026-04-20 20:42:22 +0100

    Feat: Added some styling to the hero content as well as ensuring the elements are responsive. Added Gradient button and Gradient text tsx files to the ui directory.

--- components/sections/Hero.tsx
+++ components/sections/Hero.tsx
@@ -5,19 +5,27 @@ import React from "react";
 export default function Hero() {
   return (
     <>
-      <div className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-background relative">
-        <div>
-          <h1>
+      <div className="flex w-full flex-col items-center justify-center relative border-b border-[var(--border)]">
+        <div className="z-50 flex w-full flex-col items-center justify-center py-32 relative">
+          <h1 className="text-6xl font-bold">
             <span>Rufaro</span> Ndhlovu
           </h1>
-          <div>
-            <p>Front-End Developer</p>
-            <p>
-              Building interfaces that look good, work perfectly, and feel
-              effortless on every screen, for every user.
-            </p>
-          </div>
-          <div className="absolute">RUFARO NDHLOVU</div>
+
+          <p>
+            Front-End Developer -{" "}
+            <span className="text-[var(--color-orange)]">
+              React Next.js TypeScript
+            </span>
+          </p>
+          <p className="text-wrap sm:w-2/3">
+            Building interfaces that look good, work perfectly, and feel
+            effortless on every screen, for every user.
+          </p>
+        </div>
+        <div className="absolute">
+          <p className="heronamebg font-bold text-transparent bg-clip-text text-9xl w-full z-0 text-wrap">
+            RUFARO NDHLOVU
+          </p>
         </div>
       </div>
     </>
